cd48b11dc1bac039229a7a0417b802d22d78a2bb
russo0 <38066497+russo0@users.noreply.github.com>
2026-09-26 02:00:00 +0200

    Update WebcamGallery.jsx

--- src/components/WebcamGallery.jsx
+++ src/components/WebcamGallery.jsx
@@ -15,7 +15,7 @@ const WEBCAMS = [
     country: "Sweden",
     type: "youtube",
     url: "https://www.youtube.com/embed/live_stream?channel=UCeKYE9I5zaMd4hmpoDLv47Q",
-    videoId: "UCeKYE9I5zaMd4hmpoDLv47Q"
+    videoId: "r4TjXLuWfsw"
   },
   {
     id: "Levi",
